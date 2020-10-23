ed6b6b9a12087242185e02cd2514805ae903dd40
DanilGolovin <47631010+DanilGolovin@users.noreply.github.com>
2020-10-23 12:17:18 +0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,6 @@ const  User = require("../models/user")
 const auth = async (req, res, next) => {
     //    код, который проверяет пользователя
     try {
-        //Таким образом, здесь токен const будет хранить действительное значение
         const token = req.header('Authorization').replace('Bearer ', '')//поиск заголовка, если заголовок авторизации отсутствует, заголовок будет возвращаться неопределенным
         const decoded = jwt.verify(token, process.env.JWT_SECRET)//мы хотим убедиться, что он был создан нашим сервером и срок его действия истек
         const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
